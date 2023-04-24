8577c99e06825c78042fc504597611ca4611ea0e
ttakuma17 <ttanimiya@gmail.com>
2023-04-24 18:25:29 +0900

    Dollarの副作用を確認

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -23,5 +23,7 @@ public class MoneyTest {
     Dollar five = new Dollar(5);
     five.times(2);
     assertEquals(10, five.amount);
+    five.times(3);
+    assertEquals(15, five.amount); // expected: <15> but was: <30>
   }
 }
