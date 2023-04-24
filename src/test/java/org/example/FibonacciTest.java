8b08e0890b205f164d888e2496d52758469eae56
ttakuma17 <ttanimiya@gmail.com>
2023-04-24 17:53:37 +0900

    課題の内容を確認

--- src/test/java/org/example/FibonacciTest.java
+++ src/test/java/org/example/FibonacciTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.params.ParameterizedTest.*;
-import static org.junit.jupiter.params.provider.MethodSource.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
