06f7a044b1b42b70479067154f719a5b857d6e85
ttakuma17 <ttanimiya@gmail.com>
2023-04-24 17:06:59 +0900

    TDDでfibonacciの完成

--- src/test/java/org/example/FibonacciTes.java
+++ src/test/java/org/example/FibonacciTes.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FibonacciTes {
   int fib(int n){
     if(n == 0) return 0;
-    if(n <= 2) return 1;
+    if(n == 1) return 1;
     return fib(n-1) + fib(n-2);
   }
 
