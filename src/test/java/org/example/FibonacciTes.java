8cf35d0bfbccdd7fd8a23800685d779fcac8e5e1
ttakuma17 <ttanimiya@gmail.com>
2023-04-24 16:50:24 +0900

    Fibonacci P285

---
+++ src/test/java/org/example/FibonacciTes.java
@@ -0,0 +1,15 @@
+package org.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class FibonacciTes {
+  int fib(int n){
+    return 0;
+  }
+
+  @Test
+  public void testFibonacci(){
+    assertEquals(0, fib(0));
+  }
+}
