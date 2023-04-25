2f2f0e3965b9573547e3d4ee55bcf30a99b25177
ttakuma17 <ttanimiya@gmail.com>
2023-04-25 11:07:08 +0900

    4章終わり

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -9,14 +9,16 @@ import static org.junit.jupiter.api.Assertions.*;
  * Todo
  * Unassigned
  *  $5 + 10 CHF = $10 (レートが2:1の場合)
- *  amount をprivateにする
  *  Money の丸め処理
  *  hashCode()
+ *  nullとの等価性比較
+ *  他のオブジェクトとの等価性比較
  *
  * Done
  *  $5 * 2 = $ 10
  *  Dollar の副作用
  *  equals()
+ *  amount をprivateにする
  *
  */
 public class MoneyTest {
