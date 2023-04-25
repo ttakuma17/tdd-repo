9e41215240d4ecf8eea886bc2d433341b95b16d7
ttakuma17 <ttanimiya@gmail.com>
2023-04-25 12:03:21 +0900

    Moneyクラスへdollarメソッドを追加:P48

--- src/test/java/money/Money.java
+++ src/test/java/money/Money.java
@@ -7,4 +7,8 @@ class Money {
     Money money = (Money) object;
     return amount == money.amount && getClass().equals(money.getClass());
   }
+
+  public static Dollar dollar(int amount) {
+    return new Dollar(amount);
+  }
 }
