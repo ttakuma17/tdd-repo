de2c24d8a7c1e0fb15f067afd6d4ada4d2e16a85
ttakuma17 <ttanimiya@gmail.com>
2023-04-25 12:17:05 +0900

    FactoryMethodを使うようテストコードを更新:P50

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
  *
  * Done
  *  $5 * 2 = $ 10
- *  Dollar の副作用
+ *  Money.dollar の副作用
  *  equals()
  *  amount をprivateにする
  *  5CHF * 2 = 10CHF
@@ -31,17 +31,17 @@ public class MoneyTest {
   @Test
   public void testMultiplication(){
     Money five = Money.dollar(5);
-    assertEquals(new Dollar(10), five.times(2));
-    assertEquals(new Dollar(15), five.times(3));
+    assertEquals(Money.dollar(10), five.times(2));
+    assertEquals(Money.dollar(15), five.times(3));
   }
 
   @Test
   public void testEquality(){
-    assertTrue(new Dollar(5).equals(new Dollar(5)));
-    assertFalse(new Dollar(5).equals(new Dollar(6)));
+    assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+    assertFalse(Money.dollar(5).equals(Money.dollar(6)));
     assertTrue(new Franc(5).equals(new Franc(5)));
     assertFalse(new Franc(5).equals(new Franc(6)));
-    assertFalse(new Franc(5).equals(new Dollar(5)));
+    assertFalse(new Franc(5).equals(Money.dollar(5)));
   }
 
   @Test
