cf9536a08ed4b71f03d09d4670afe68de8f6d33a
ttakuma17 <ttanimiya@gmail.com>
2023-04-26 10:39:30 +0900

    currency関連の実装を親クラスへ引き上げ:p58

--- src/test/java/money/Dollar.java
+++ src/test/java/money/Dollar.java
@@ -2,14 +2,9 @@ package money;
 
 public class Dollar extends Money {
 
-  private String currency;
   Dollar(int amount) {
     this.amount = amount;
     currency = "USD";
-  }
-
-  String currency(){
-    return currency;
   }
 
   Money times(int multiplier) {
