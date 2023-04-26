e762d9a14c20cf62af562ca64d94eadf640b101f
ttakuma17 <ttanimiya@gmail.com>
2023-04-26 10:42:53 +0900

    FactoryMethodを親クラスへ移動させる:P59

--- src/test/java/money/Franc.java
+++ src/test/java/money/Franc.java
@@ -2,12 +2,12 @@ package money;
 
 class Franc extends Money{
 
-  Franc(int amount) {
+  Franc(int amount, String currency) {
     this.amount = amount;
-    currency = "CHF";
+    this.currency = "CHF";
   }
 
   Money times(int multiplier) {
-    return new Franc(amount * multiplier);
+    return new Franc(amount * multiplier, null);
   }
 }
