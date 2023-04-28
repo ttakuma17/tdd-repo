60d899e42f128786b0b19c51535b4a4541fe5b12
ttakuma17 <ttanimiya@gmail.com>
2023-04-28 15:49:40 +0900

    reduceメソッドをExpressionへ引き上げる

--- src/test/java/money/Bank.java
+++ src/test/java/money/Bank.java
@@ -2,7 +2,7 @@ package money;
 
 public class Bank {
   Money reduce(Expression source, String to){
-    if (source instanceof Money) return (Money) source;
+    if (source instanceof Money) return ((Money) source).reduce(to);
     Sum sum = (Sum)source;
     return sum.reduce(to);
   }
